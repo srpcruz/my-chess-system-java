fcc68cccc7749e234bfc954781c8587e75718dac
srpcruz <srcruz15@gmail.com>
2021-07-05 15:17:31 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	// "protected" fará que metodos e parametros sejam
 	// visiveis somente neste metodo, suas subclasses e 
@@ -19,5 +19,22 @@ public class Piece {
 		return board;
 	}
 	
+	public abstract boolean[][] possibleMoves();
 	
+	// rook method - metodo concreto executando um metodo abstrato....
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0;i<mat.length;i++) {
+			for (int j=0;j<mat.length;j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
