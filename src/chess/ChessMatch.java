5a2326ab38b1559c0fa74194ea295acf57811e04
srpcruz <srcruz15@gmail.com>
2021-07-07 18:45:45 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -119,7 +119,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.RemovePiece(source);
+		ChessPiece p = (ChessPiece)board.RemovePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.RemovePiece(target);
 		board.PlacePiece(p, target);
 		
@@ -133,7 +134,8 @@ public class ChessMatch {
 	
 	// metodo para desfazer o movimento makeMove
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.RemovePiece(target);
+		ChessPiece p = (ChessPiece)board.RemovePiece(target);
+		p.decreaseMoveCount();
 		board.PlacePiece(p, source);
 		
 		if (capturedPiece != null) {
